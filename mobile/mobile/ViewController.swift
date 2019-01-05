4263e33d56ee4fb29f4016752e0a542c560cdc44
Nadir Muzaffar <nadirabid@gmail.com>
2019-01-05 10:29:33 -0800

    init mobile project

---
+++ mobile/mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile
+//
+//  Created by Nadir Muzaffar on 12/29/18.
+//  Copyright © 2018 Nadir Muzaffar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
