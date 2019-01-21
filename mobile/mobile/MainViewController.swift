8a8f3c52ba7bcd4613909ae3c5304d5faf6b989c
Nadir Muzaffar <nadirabid@gmail.com>
2019-01-20 20:18:29 -0800

    table viewwwwww

---
+++ mobile/mobile/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  mobile
+//
+//  Created by Nadir Muzaffar on 12/29/18.
+//  Copyright © 2018 Nadir Muzaffar. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var items = ["one", "two", "three" ]
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorkoutCell", for: indexPath)
+        
+        if let workoutTableViewCell = cell as? WorkoutTableViewCell {
+            workoutTableViewCell.workoutName.text = items[indexPath.row]
+        }
+        
+        return cell
+    }
+}
---
+++ mobile/mobile/WorkoutTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WorkoutTableViewCell.swift
+//  mobile
+//
+//  Created by Nadir Muzaffar on 1/20/19.
+//  Copyright © 2019 Nadir Muzaffar. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+    @IBOutlet weak var workoutName: UILabel!
+    
+
+}
