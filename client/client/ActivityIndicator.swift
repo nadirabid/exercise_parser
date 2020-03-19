093150fc545f0c3469a1081e90161601092a6a53
Nadir Muzaffar <nadirabid@gmail.com>
2020-03-18 23:47:43 -0700

    on finish - save and conditionally show actual location

---
+++ client/client/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityIndicator.swift
+//  client
+//
+//  Created by Nadir Muzaffar on 3/18/20.
+//  Copyright © 2020 Nadir Muzaffar. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
