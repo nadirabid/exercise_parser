e31d45cbdbd4d085320eb73f16f9bdc46578d6bf
Nadir Muzaffar <nadirabid@gmail.com>
2020-04-09 14:08:01 -0700

    editor refinements - primarily animations - few remain

---
+++ client/client/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  client
+//
+//  Created by Nadir Muzaffar on 4/9/20.
+//  Copyright © 2020 Nadir Muzaffar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Extensions_Previews: PreviewProvider {
+    static var previews: some View {
+        Extensions()
+    }
+}
---
+++ client/client/ViewModifiers.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModifiers.swift
+//  client
+//
+//  Created by Nadir Muzaffar on 4/9/20.
+//  Copyright © 2020 Nadir Muzaffar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModifiers()
+    }
+}
