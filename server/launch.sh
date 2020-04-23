c2d7b2822556736c3655e1c68e58c5d73e7b6877
Nadir Muzaffar <nadirabid@gmail.com>
2020-04-23 14:27:38 -0700

    add seed/migrate dict before launching in dockerfile

---
+++ server/launch.sh
@@ -0,0 +1,2 @@
+/go/bin/exercise_parser db seed dict --conf=/go/src/exercise_parser/server/conf/${CONFIG}.toml
+/go/bin/exercise_parser start --conf=/go/src/exercise_parser/server/conf/${CONFIG}.toml
