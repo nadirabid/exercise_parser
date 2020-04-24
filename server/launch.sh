fb40a8b3e09fe7b4c2d2a73d21a3967f96bab6ec
Nadir Muzaffar <nadirabid@gmail.com>
2020-04-24 13:46:03 -0700

    disable migration for a bit

--- server/launch.sh
+++ server/launch.sh
@@ -1,2 +1,4 @@
-/go/bin/exercise_parser db seed dict --conf=/go/src/exercise_parser/server/conf/${CONFIG}.toml
+#!/bin/bash
+
+# /go/bin/exercise_parser db seed dict --conf=/go/src/exercise_parser/server/conf/${CONFIG}.toml
 /go/bin/exercise_parser start --conf=/go/src/exercise_parser/server/conf/${CONFIG}.toml
