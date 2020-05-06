7348c2ab167b9ec2a2e74b20c6190a21fb349d35
Nadir Muzaffar <nadirabid@gmail.com>
2020-05-06 13:03:38 -0700

    fix user_follow migration

--- server/migrations/000001_init.up.sql
+++ server/migrations/000001_init.up.sql
@@ -13,7 +13,6 @@ CREATE TABLE IF NOT EXISTS exercise_dictionaries (
     tsv tsvector
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS exercise_dictionaries_pkey ON exercise_dictionaries(id int4_ops);
 CREATE UNIQUE INDEX IF NOT EXISTS exercise_dictionaries_name_key ON exercise_dictionaries(name text_ops);
 CREATE INDEX IF NOT EXISTS idx_exercise_dictionaries_deleted_at ON exercise_dictionaries(deleted_at timestamptz_ops);
 
@@ -50,7 +49,6 @@ CREATE TABLE IF NOT EXISTS muscles (
     exercise_dictionary_id integer REFERENCES exercise_dictionaries(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS muscles_pkey ON muscles(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_muscles_deleted_at ON muscles(deleted_at timestamptz_ops);
 
 -- Classification ----------------------------------------------
@@ -70,7 +68,6 @@ CREATE TABLE IF NOT EXISTS classifications (
     exercise_dictionary_id integer REFERENCES exercise_dictionaries(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS classifications_pkey ON classifications(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_classifications_deleted_at ON classifications(deleted_at timestamptz_ops);
 
 -- Articulation ----------------------------------------------
@@ -83,7 +80,6 @@ CREATE TABLE IF NOT EXISTS articulations (
     exercise_dictionary_id integer REFERENCES exercise_dictionaries(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS articulations_pkey ON articulations(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_articulations_deleted_at ON articulations(deleted_at timestamptz_ops);
 
 -- Joints ----------------------------------------------
@@ -110,7 +106,6 @@ CREATE TABLE IF NOT EXISTS joints (
     articulation_id integer REFERENCES articulations(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS joints_pkey ON joints(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_joints_deleted_at ON joints(deleted_at timestamptz_ops);
 
 -- User ----------------------------------------------
@@ -126,7 +121,6 @@ CREATE TABLE IF NOT EXISTS users (
     external_user_id text NOT NULL
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS users_pkey ON users(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_users_deleted_at ON users(deleted_at timestamptz_ops);
 CREATE UNIQUE INDEX IF NOT EXISTS ext_id ON users(external_user_id text_ops);
 
@@ -143,7 +137,6 @@ CREATE TABLE IF NOT EXISTS workouts (
     user_id integer REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS workouts_pkey ON workouts(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_workouts_deleted_at ON workouts(deleted_at timestamptz_ops);
 
 -- Location ----------------------------------------------
@@ -158,7 +151,6 @@ CREATE TABLE IF NOT EXISTS locations (
     workout_id integer REFERENCES workouts(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS locations_pkey ON locations(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_locations_deleted_at ON locations(deleted_at timestamptz_ops);
 
 -- Exercise ----------------------------------------------
@@ -176,7 +168,6 @@ CREATE TABLE IF NOT EXISTS exercises (
     workout_id integer REFERENCES workouts(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS exercises_pkey ON exercises(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_exercises_deleted_at ON exercises(deleted_at timestamptz_ops);
 
 -- ExerciseData ----------------------------------------------
@@ -194,7 +185,6 @@ CREATE TABLE IF NOT EXISTS exercise_data (
     exercise_id integer REFERENCES exercises(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS exercise_data_pkey ON exercise_data(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_exercise_data_deleted_at ON exercise_data(deleted_at timestamptz_ops);
 
 -- DistanceExercise ----------------------------------------------
@@ -209,7 +199,6 @@ CREATE TABLE IF NOT EXISTS distance_exercises (
     exercise_id integer REFERENCES exercises(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS distance_exercises_pkey ON distance_exercises(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_distance_exercises_deleted_at ON distance_exercises(deleted_at timestamptz_ops);
 
 -- WeightedExercise ----------------------------------------------
@@ -225,7 +214,6 @@ CREATE TABLE IF NOT EXISTS weighted_exercises (
     exercise_id integer REFERENCES exercises(id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS weighted_exercises_pkey ON weighted_exercises(id int4_ops);
 CREATE INDEX IF NOT EXISTS idx_weighted_exercises_deleted_at ON weighted_exercises(deleted_at timestamptz_ops);
 
 COMMIT;
--- server/migrations/000002_create_user_follow.up.sql
+++ server/migrations/000002_create_user_follow.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS user_follows (
     updated_at timestamp with time zone,
     deleted_at timestamp with time zone,
     user_id integer REFERENCES users(id) ON DELETE CASCADE,
-    follow_id integer REFERENCES user(id) ON DELETE CASCADE
+    follow_id integer REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX idx_user_id_and_follow_id ON user_follows(user_id, follow_id);
