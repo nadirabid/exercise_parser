5a6d64e42c61ae1f22c0197ed1e81333fee967b8
Nadir Muzaffar <nadirabid@gmail.com>
2020-05-18 20:55:39 -0700

    add utility to delete/recompute metrics

--- server/cmd/metrics.go
+++ server/cmd/metrics.go
@@ -38,6 +38,36 @@ func computeMetricsForAllWorkouts(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
+// delete and recompute all metrics
+func recomputeMetricsForAllWorkouts(cmd *cobra.Command, args []string) error {
+	v, err := configureViperFromCmd(cmd)
+	if err != nil {
+		return err
+	}
+
+	db, err := models.NewDatabase(v)
+	if err != nil {
+		return err
+	}
+
+	err = db.
+		Where(`workouts.id NOT IN (SELECT metrics.workout_id FROM metrics)`).
+		Delete(models.Workout{}).
+		Error
+
+	if err != nil {
+		return err
+	}
+
+	return computeMetricsForAllWorkouts(cmd, args)
+}
+
+var recomputeMetricsCmd = &cobra.Command{
+	Use:   "recompute",
+	Short: "Recompute metrics for all workouts",
+	RunE:  recomputeMetricsForAllWorkouts,
+}
+
 var computeMetricsCmd = &cobra.Command{
 	Use:   "compute",
 	Short: "Compute metrics for all workouts",
@@ -53,4 +83,5 @@ func init() {
 	rootCmd.AddCommand(metricsCmd)
 
 	metricsCmd.AddCommand(computeMetricsCmd)
+	metricsCmd.AddCommand(recomputeMetricsCmd)
 }
