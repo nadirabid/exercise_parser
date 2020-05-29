f370ed3968885fb5c997715944a64f87fe4e59fb
Nadir Muzaffar <nadirabid@gmail.com>
2020-05-28 17:20:20 -0700

    app store requirement: marketing page

--- web/main/src/App.js
+++ web/main/src/App.js
@@ -1,24 +1,41 @@
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    backgroundColor: theme.palette.app.primary,
+  },
+  mainText: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  typoHeaderPrimary: {
+    letterSpacing: '0.2em',
+    fontWeight: theme.typography.fontWeightBold,
+    color: '#fff',
+  },
+  typoHeaderSecondary: {
+    fontWeight: theme.typography.fontWeightBold,
+    color: theme.palette.app.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+        <div className={classes.mainText}>
+          <Typography className={classes.typoHeaderSecondary} variant="h4" noWrap={true} fontWeight="fontWeightBold">FOR THE ATHLETES</Typography>
+          <Typography className={classes.typoHeaderPrimary} variant="h1" noWrap={true} fontWeight="fontWeightBold">RYDEN</Typography>
+          <Typography className={classes.typoHeaderSecondary} variant="h4" noWrap={true} fontWeight="fontWeightBold">FORM & WILL</Typography>
+        </div>
     </div>
   );
 }
--- web/main/src/index.js
+++ web/main/src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  palette: {
+    app: {
+      primary: '#F8481C',
+      secondary: '#F43605',
+    },
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
